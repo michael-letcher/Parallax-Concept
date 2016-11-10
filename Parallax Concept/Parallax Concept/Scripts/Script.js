8549ad57c7e1d182477d649c12caeba5cd5a1811
Michael Letcher <Michael.Letcher@coliban.com.au>
2016-11-10 18:42:15 +1100

    fixed the scroll and started theme

---
+++ Parallax Concept/Parallax Concept/Scripts/Script.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+    $(window).scroll(function () {
+        //Nav bar will stick to the top when scolled past a point. 
+
+        //console.log($(window).scrollTop())
+        if ($(window).scrollTop() > 201) {
+            $('nav').addClass('navbar-fixed-top');
+            $('.navbar-header a').show();
+        }
+        if ($(window).scrollTop() < 200) {
+            $('nav').removeClass('navbar-fixed-top');
+            $('.navbar-header a').hide();
+        }
+    });
+
+    //Watch clicks on navigation
+    $('.nav').on('click', 'li', function () {
+        console.log($(this).text());
+        $('li').removeClass('active');
+        $(this).addClass('active');
+    });
+});
