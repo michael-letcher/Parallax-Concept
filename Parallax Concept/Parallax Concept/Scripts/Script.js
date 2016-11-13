934e4dd626cb9d339c35d9894a39737b9e173dc3
Michael Letcher <Michael.Letcher@coliban.com.au>
2016-11-14 09:08:55 +1100

    added particles, fixed bug with nav scoll, centered nav when not fixed to top

--- Parallax Concept/Parallax Concept/Scripts/Script.js
+++ Parallax Concept/Parallax Concept/Scripts/Script.js
@@ -6,10 +6,12 @@
         //console.log($(window).scrollTop())
         if ($(window).scrollTop() > 201) {
             $('nav').addClass('navbar-fixed-top');
+            $('nav').removeClass('navbar-unfixed');
             $('.navbar-header a').show();
         }
         if ($(window).scrollTop() < 200) {
             $('nav').removeClass('navbar-fixed-top');
+            $('nav').addClass('navbar-unfixed');
             $('.navbar-header a').hide();
         }
     });
@@ -19,5 +21,10 @@
         console.log($(this).text());
         $('li').removeClass('active');
         $(this).addClass('active');
+    });
+
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles', 'particles-config.json', function () {
+        console.log('callback - particles.js config loaded');
     });
 });
